8a2babdf434137cab476a119e51df0ca917acde1
Moatassem Waly <m.waly@saedubai.com>
2016-07-13 01:59:31 +0400

    V2
    Title Text Done

---
+++ Endless Nights/Assets/Scripts/TextFade.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextFade : MonoBehaviour
+{
+    public float time = 5; //Seconds to read the text
+
+    void Start()
+    {
+        Destroy(gameObject, time);
+    }
+}
